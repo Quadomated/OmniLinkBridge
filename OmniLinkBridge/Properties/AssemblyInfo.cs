94f5d6efc13c3b8642e19dcfc82872399dc1cc96
Ryan Wagoner <rwagoner@excalibur-partners.com>
2020-01-05 21:34:03 -0500

    1.1.9 - Switch to structured logging and add telemetry

--- OmniLinkBridge/Properties/AssemblyInfo.cs
+++ OmniLinkBridge/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OmniLinkBridge")]
+[assembly: AssemblyTitle("OmniLink Bridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Excalibur Partners, LLC")]
 [assembly: AssemblyProduct("OmniLinkBridge")]
-[assembly: AssemblyCopyright("Copyright © Excalibur Partners, LLC 2019")]
+[assembly: AssemblyCopyright("Copyright © Excalibur Partners, LLC 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.8.0")]
-[assembly: AssemblyFileVersion("1.1.8.0")]
+[assembly: AssemblyVersion("1.1.9.0")]
+[assembly: AssemblyFileVersion("1.1.9.0")]
